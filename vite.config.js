bdfc175164d230f8ab10b39f22db94e9ea51653d
ChandraKarthik07 <99110756+ChandraKarthik07@users.noreply.github.com>
2025-07-30 22:44:21 +0530

    Final local changes before push

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Taskmaster-Pro/', // 👈 use your GitHub repo name here (case-sensitive)
-});
+  base: "/Taskmaster-Pro/", // 🔥 important for GitHub Pages
+})
